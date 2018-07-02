991ed220f0b4219c34f4dad49f54e0db9d085622
huhuibin147 <405622418@qq.com>
2018-07-02 18:08:24 +0800

    add

---
+++ localtest.py
@@ -0,0 +1,17 @@
+#coding:utf8
+import sys
+import json
+import requests
+
+
+centerURL = 'http://127.0.0.1:10003/'
+
+cmd = 'bp'
+
+if len(sys.argv) > 1:
+    msg = sys.argv[1]
+
+data = {"qqid": "405622418"}
+res = requests.post(centerURL+cmd, data=data)
+
+print(res.text)
