25c68d32152786e7751e2f2f5003a4e1121cc911
huhuibin147 <405622418@qq.com>
2018-08-13 15:01:17 +0800

    日志添加轮转时间

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 import sys
 import yaml
+import os
 import re
 import logging
+from logging.handlers import TimedRotatingFileHandler
 # from tornado.wsgi import WSGIContainer
 # from tornado.httpserver import HTTPServer
 # from tornado.ioloop import IOLoop
@@ -16,13 +18,37 @@ if len(sys.argv) < 2:
     print('usage: {python3 main.py app}')
     exit()
 
+def addTimedRotatingFileHandler(filename, **kwargs):
+    """给logger添加一个时间切换文件的handler
+    默认时间是0点，3个备份
+    """
+    dname = os.path.dirname(filename)
+    if dname and not os.path.isdir(dname):
+        os.makedirs(dname, '0755')
+    conf = {
+        'when': 'midnight',
+        'backupCount': 3,
+        'format': '[%(asctime)s][tid:%(thread)d][%(filename)s:%(lineno)d] %(levelname)s: %(message)s',
+        'logger': logging.getLogger(),
+    }
+    conf.update(kwargs)
+    if 'logLevel' in conf:
+        if isinstance(conf['logLevel'], str):
+            conf['logLevel'] = getattr(logging, conf['logLevel'])
+        conf['logger'].setLevel(conf['logLevel'])
+    handler = logging.handlers.TimedRotatingFileHandler(
+        filename = filename,
+        when = conf['when'],
+        backupCount = conf['backupCount'],
+    )
+    handler.setFormatter(
+        logging.Formatter(conf['format'])
+    )
+    conf['logger'].addHandler(handler)
 
-# 初始化日志格式
-logging.basicConfig(
-    level=logging.INFO,
-    format="[%(asctime)s][%(levelname).4s][%(name)s] %(message)s",
-    datefmt='%Y-%m-%d %H:%M:%S'
-)
+# 日志轮转初始化
+addTimedRotatingFileHandler('./var/log/itb.%s.log' % appid, logLevel = logging.INFO)
+
 
 def refLoadYaml(filename):
     p = re.compile('\d')
