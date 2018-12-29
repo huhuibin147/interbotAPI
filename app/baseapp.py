a6d840d62e24f2fe31f5c0195d4b12c8c497fd8b
huhuibin147 <405622418@qq.com>
2018-12-29 12:26:26 +0800

    add apiv2 scope

--- app/baseapp.py
+++ app/baseapp.py
@@ -95,7 +95,7 @@ def chat(**kw):
 @appTools.deco()
 def bindUserToken(**kw):
     ins = baseHandler.baseHandler()
-    url = 'https://osu.ppy.sh/oauth/authorize?client_id=19&response_type=code&state=%sx%s' % (kw['qqid'], kw['groupid'])
+    url = 'https://osu.ppy.sh/oauth/authorize?client_id=19&response_type=code&state=%sx%s&scope=friends.read%%20identify' % (kw['qqid'], kw['groupid'])
     ret = url + '\n*不要点别人的链接!，不要点别人的链接!'
     return ret
 
