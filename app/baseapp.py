77cf376392c96ea54b087ddd10ac6da046ac1455
unknown <405622418@qq.com>
2019-02-15 20:23:17 +0800

    oauth文案修改

--- app/baseapp.py
+++ app/baseapp.py
@@ -97,7 +97,7 @@ def chat(**kw):
 def bindUserToken(**kw):
     ins = baseHandler.baseHandler()
     url = 'https://osu.ppy.sh/oauth/authorize?client_id=19&response_type=code&state=%sx%s&scope=friends.read%%20identify' % (kw['qqid'], kw['groupid'])
-    ret = url + '\n*不要点别人的链接!，不要点别人的链接!'
+    ret = url + '\n*请点击链接进行授权绑定'
     return ret
 
 @app.route('/permission', methods=['POST'])
