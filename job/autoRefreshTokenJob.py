4068855ccd1e80f3e691cb2228679130778dce2f
huhuibin147 <405622418@qq.com>
2019-04-29 11:22:39 +0800

    定时刷新ppy token

---
+++ job/autoRefreshTokenJob.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+import time
+import logging
+import jobCenter
+import interMysql
+
+
+from ppyappLib import ppyAPI
+
+
+class autoRefreshTokenJob(jobCenter.jobCenter):
+
+
+    def addJob(self):
+        self.sched.add_job(self.jobMethod, 'interval', minutes=60, args=[])
+
+    def jobMethod(self):
+        # get users
+        users = self.getUsers()
+        # refresh token
+        for u in users:
+            logging.info('dealing.. %s', u)
+            ppyAPI.apiv2RefreshToken(u["refreshtoken"], qq=u["qq"], groupid=u["groupid"])
+
+    def getUsers(self):
+        db = interMysql.Connect('osu2')
+        sql = '''
+            SELECT qq, groupid, acesstoken, refreshtoken 
+            FROM user where acesstoken!='' group by qq
+        '''
+        ret = db.query(sql)
+        return ret
--- job/jobCenter.py
+++ job/jobCenter.py
@@ -1,26 +1,60 @@
 # -*- coding: utf-8 -*-
+import os
 import sys
 import json
 import logging
+from logging.handlers import TimedRotatingFileHandler
 import websocket
 
+sys.path.append('./')
 sys.path.append('./commLib')
 
 import pushTools
 from apscheduler.schedulers.blocking import BlockingScheduler
 
+appid = sys.argv[1]
 
+def addTimedRotatingFileHandler(filename, **kwargs):
+    """给logger添加一个时间切换文件的handler
+    默认时间是0点，3个备份
+    """
+    dname = os.path.dirname(filename)
+    if dname and not os.path.isdir(dname):
+        os.makedirs(dname, '0755')
+    conf = {
+        'when': 'midnight',
+        'backupCount': 3,
+        'format': '[%(asctime)s][tid:%(thread)d][%(filename)s:%(lineno)d] %(levelname)s: %(message)s',
+        'logger': logging.getLogger(),
+    }
+    conf.update(kwargs)
+    if 'logLevel' in conf:
+        if isinstance(conf['logLevel'], str):
+            conf['logLevel'] = getattr(logging, conf['logLevel'])
+        conf['logger'].setLevel(conf['logLevel'])
+    handler = logging.handlers.TimedRotatingFileHandler(
+        filename = filename,
+        when = conf['when'],
+        backupCount = conf['backupCount'],
+    )
+    handler.setFormatter(
+        logging.Formatter(conf['format'])
+    )
+    conf['logger'].addHandler(handler)
+
+# 日志轮转初始化
+addTimedRotatingFileHandler('./var/log/sitb.%s.log' % appid, logLevel = logging.INFO)
 
 class jobCenter():
 
 
     def __init__(self):
         self.sched = BlockingScheduler()
-        logging.basicConfig(
-            level=logging.INFO,
-            format='[%(asctime)s][%(levelname)s]%(message)s',
-            datefmt='%Y-%m-%d %H:%M:%S'
-        )
+        # logging.basicConfig(
+        #     level=logging.INFO,
+        #     format='[%(asctime)s][%(levelname)s]%(message)s',
+        #     datefmt='%Y-%m-%d %H:%M:%S'
+        # )
 
     def start(self):
         self.addJob()
