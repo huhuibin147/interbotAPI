b8da665d950be1667a57b6680c05fe6c1452628e
huhuibin147 <405622418@qq.com>
2019-07-06 11:20:47 +0800

    拉起监控

--- loopUtil2.sh
+++ loopUtil2.sh
@@ -10,7 +10,14 @@ checkalive() {
         # echo $lines
         sh sitb.sh $1 restart
     fi
+
+    lines=`ps axu|grep "python3 job/jobCenter.py $1"|grep -v grep|wc -l`
+    if [ "1" -ne $lines ]; then
+        #echo $lines
+        sh sitb.sh $1 restart
+    fi
 } 
 
 
 checkalive $1
+
